b476c2cff7d84b05426ea3dc63c01ec1c32144b9
GabrieleFortunato <gabrielefortunato78@libero.it>
2017-10-04 13:51:51 +0200

    Commit del 4 ottobre 2017 ore 13,51

--- fibonacci/src/fibonacci.h
+++ fibonacci/src/fibonacci.h
@@ -8,22 +8,6 @@
 #ifndef FIBONACCI_H_
 #define FIBONACCI_H_
 
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ZERO       		  0
-#define ONE        		  1
-#define TWO        		  2
-#define NUMBERS_FOR_LINE  4
-#define NUMBERS			  40
-#define FILE_NAME         "fibonacci.txt"
-#define FILE_MODE  		  "w"
-#define NEW_LINE          "\n"
-#define SEPARATOR_LINE    "-"
-#define PRINT_NUMBER      "Posizione numero %2d: numero %9d"
-#define PRINT_SERIE       "SERIE DI FIBONACCI"
-
 /**
  * Restituisce il numero appartenente alla serie di Fibonacci
  * e corrispondente alla posizione indicata dal parametro position
